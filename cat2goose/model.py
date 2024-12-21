f7601bb8ef92aceed999b46143b7bb0322d4501b
poly000 <1348292515@qq.com>
2024-12-22 00:00:51 +0800

    refactor: refactor code

---
+++ cat2goose/model.py
@@ -0,0 +1,5 @@
+class GooseRule:
+    def __init__(self, rule_type: str, content: str, target_group: str):
+        self.rule_type = rule_type
+        self.content = content
+        self.target_group = target_group
